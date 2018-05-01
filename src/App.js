4192d57937f75aeff1bb4cce9c4e798563f83cdf
Alina <alina.chernavskikh@gmail.com>
2018-05-01 21:47:50 +0300

    Add simple examples

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NameList from './components/NameList';
+import Hrefs from './components/Hrefs';
+import Input from './components/Input';
 
 class App extends Component {
   getText() {
@@ -12,6 +14,10 @@ class App extends Component {
     return (
       <div className="App">
         <NameList />
+        <hr/>
+        <Hrefs />
+        <hr/>
+        <Input />
       </div>
     );
   }
---
+++ src/components/Hrefs.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Hrefs extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hrefs: [
+                { href: '1.html', text: 'link1' },
+                { href: '2.html', text: 'link2' },
+                { href: '3.html', text: 'link3' },
+            ],
+        };
+    }
+
+    render() {
+        const list = this.state.hrefs.map((item, index) => {
+            return <a style={{display: 'block', paddingLeft: '20px'}}key={index} href={item.href}>{item.text}</a>;
+        });
+        return <div style={{textAlign: 'left'}}>
+                Links:
+                {list}
+            </div>;
+    }
+}
